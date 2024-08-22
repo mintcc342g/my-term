b953645f7f8c2c0880a80f80df1c6a5f4d29a204
mintcc342g <52809740+mintcc342g@users.noreply.github.com>
2024-08-22 17:19:36 +0900

    add install.sh and edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# my-term-conf
+# my-term
+
+터미널 개인 설정
+
+## 사전 작업
+- 서체 설치
+- iTerm2 설치 및 프로필 설정
+
+## 설치 프로그램 목록
+- oh-my-zsh
+- nord theme (리눅스 한정)
+- newro theme
+- brew
+  - zsh-autosuggestions
+  - zsh-syntax-highlighting
+  - ripgrep
+  - tree
+  - maccy
+  - rectangle
+
+## 각 프로그램 설정값
+### iTerm2
+- 한글 자소분리 해결
+  1. Settings
+  2. Profiles
+  3. 설정한 프로필 선택
+  4. Text 탭 클릭
+  5. Unicode normalization form 을 HFS+ 로 변경
+  6. iTerm2 종료 후 재구동
+
+### maccy
+- TODO
+
+### rectangle
+- TODO
---
+++ install.sh
@@ -0,0 +1,41 @@
+
+#!/bin/bash
+cd $HOME
+
+if [ "$(uname -s)" = "Linux" ]; then
+    OS="Linux"
+    mkdir "Documents"
+else
+    OS=""
+fi
+
+echo "\033[33;1m→\033[0m install oh-my-zsh…"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "\033[33;1m→\033[0m install brew…"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "\033[33;1m→\033[0m install useful features with Homebrew…"
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install ripgrep
+brew install tree
+brew install maccy
+brew install rectangle
+
+echo "\033[33;1m→\033[0m add shell startup settings…"
+echo -e "\n# Homebrew 설정\neval \"\$(/opt/homebrew/bin/brew shellenv)\"" >> ./.zshrc
+echo -e "\n# zsh-syntax-highlighting 설정\nsource /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ./.zshrc
+echo -e "\n# zsh-autosuggestions 설정\nsource $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ./.zshrc
+
+echo "\033[33;1m→\033[0m install newro theme…"
+git clone https://gitlab.com/newrovp/develconfig.git ./Documents/newrovp
+cp ./Documents/newrovp/newro_vcs.zsh-theme ./.oh-my-zsh/themes/newro_vcs.zsh-theme
+sed -i -E 's/robbyrussell/newro_vcs/g' ./.zshrc
+
+if [ "$OS" != "" ]; then
+    echo "\033[33;1m→\033[0m install nord colors…"
+    git clone https://github.com/nordtheme/dircolors.git ./custom_colors/nord_color
+    ln -srfv "./custom_colors/nord_color/src/dir_colors" ./.dir_colors
+    echo -e "\n# nord color 설정\neval \"\$(dircolors ./.dir_colors)\"" >> ./.zshrc
+fi
