e17229facb0439757d8f31e84ef9985d88fffcf0
mintcc342g <52809740+mintcc342g@users.noreply.github.com>
2024-08-23 18:10:41 +0900

    fix brew prefix

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ cd $HOME
 
 if [ "$(uname -s)" = "Linux" ]; then
     OS="Linux"
-    mkdir "Documents"
+    echo "\033[31;1mNOT SUPPORT OS\033[0m…"
+    exit 0
 else
     OS=""
 fi
@@ -14,8 +15,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 echo "\033[33;1m→\033[0m install brew…"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ./.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+brew_prefix=$(brew --prefix)
+echo "eval \"\$($brew_prefix/bin/brew shellenv)\"" >> ./.zprofile
+eval "$($brew_prefix/bin/brew shellenv)"
 
 echo "\033[33;1m→\033[0m install useful features with Homebrew…"
 brew install zsh-autosuggestions
@@ -25,19 +27,20 @@ brew install tree
 brew install maccy
 brew install rectangle
 
-echo "\033[33;1m→\033[0m add shell startup settings…"
-echo -e "\n# Homebrew 설정\neval \"\$(/opt/homebrew/bin/brew shellenv)\"" >> ./.zshrc
-echo -e "\n# zsh-syntax-highlighting 설정\nsource /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ./.zshrc
-echo -e "\n# zsh-autosuggestions 설정\nsource $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ./.zshrc
+echo -e "\033[33;1m→\033[0m add shell startup settings…"
+echo "\n# Homebrew 설정\neval \"\$($brew_prefix/bin/brew shellenv)\"" >> ./.zshrc
+echo "\n# zsh-syntax-highlighting 설정\nsource $brew_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ./.zshrc
+echo "\n# zsh-autosuggestions 설정\nsource $brew_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ./.zshrc
 
 echo "\033[33;1m→\033[0m install newro theme…"
 git clone https://gitlab.com/newrovp/develconfig.git ./Documents/newrovp
 cp ./Documents/newrovp/newro_vcs.zsh-theme ./.oh-my-zsh/themes/newro_vcs.zsh-theme
 sed -i -E 's/robbyrussell/newro_vcs/g' ./.zshrc
 
-if [ "$OS" != "" ]; then
-    echo "\033[33;1m→\033[0m install nord colors…"
-    git clone https://github.com/nordtheme/dircolors.git ./custom_colors/nord_color
-    ln -srfv "./custom_colors/nord_color/src/dir_colors" ./.dir_colors
-    echo -e "\n# nord color 설정\neval \"\$(dircolors ./.dir_colors)\"" >> ./.zshrc
-fi
+# TODO: for linux
+# if [ "$OS" != "" ]; then
+#     echo "\033[33;1m→\033[0m install nord colors…"
+#     git clone https://github.com/nordtheme/dircolors.git ./custom_colors/nord_color
+#     ln -srfv "./custom_colors/nord_color/src/dir_colors" ./.dir_colors
+#     echo -e "\n# nord color 설정\neval \"\$(dircolors ./.dir_colors)\"" >> ./.zshrc
+# fi
